e13d7fa71870690e0b09dfaef6c31db5253e507c
Mustafa El Menshawy <mustafa1elmenshawy@gmail.com>
2016-12-17 15:58:32 +0200

    newwwwwww

--- edugame/src/edugame/BuilderControl.java
+++ edugame/src/edugame/BuilderControl.java
@@ -5,19 +5,39 @@
  */
 package edugame;
 
+import java.io.IOException;
+
 /**
  *
  * @author musta
  */
 public class BuilderControl {
-  public static boolean CheckDuplicate(String gameName , String categoryName){
+       
+  public static boolean CheckDuplicate(String gameName , String categoryName) throws IOException, ClassNotFoundException{
       
-      return true;
+      return GameDBModel.GameQuery(gameName, categoryName);
   }
-  public static void CreateGame(String gameName , int levelNo , String type){
+  public static void CreateGame(String gameName , int levelNo , String type ,Teacher Owner ){
+    Game  game = new Game(levelNo, gameName , type ,Owner );
+    for(int i=0;i<levelNo;i++ ){
+        if(type .equals( "MCQ")){
+            for(int j=0;j<4;j++){
+                MCQ lv= new MCQ(GameBuilder.GetName(),GameBuilder.GetQuestion(),GameBuilder.GetRightAnswer());
+                Answer ans = new Answer(HomePage.GetAnswer(),HomePage.GetImage());
+                lv.setAnswers(ans);
+            }
+            game.setListofLevels(ans);
+            
+        }
+        else{
+             TorF lv= new TorF(GameBuilder.GetName(),GameBuilder.GetQuestion(),GameBuilder.GetRightAnswer());
+        }
+    }
+      
+      
+      
+      
       
   }
-  public static void CreateAnswer(String image , String answer){
-      
-  }
+
 }
