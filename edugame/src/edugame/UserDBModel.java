d9e12e2633f2d244cb26ccc3d84d78eca22ac1a6
IsmailAhmedIsmail <ismail.ahmed2014@stud.fci-cu.edu.eg>
2016-12-18 01:01:09 +0200

    Fixed Corrupted Stream Exception
    Yet Null values remaining

--- edugame/src/edugame/UserDBModel.java
+++ edugame/src/edugame/UserDBModel.java
@@ -6,19 +6,32 @@
 package edugame;
 
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
 /**
  *
  * @author IsmailAhmed
  */
 public class UserDBModel {
+  static class AppendingObjectOutputStream extends ObjectOutputStream {
 
+  public AppendingObjectOutputStream(OutputStream out) throws IOException {
+    super(out);
+  }
+
+  @Override
+  protected void writeStreamHeader() throws IOException {
+    reset();
+  }
+
+}
     public UserDBModel() {
     }
 
@@ -40,10 +53,18 @@ public class UserDBModel {
     }
 
     public static void add(User u) throws FileNotFoundException, IOException {
-        ObjectOutputStream f = new ObjectOutputStream(new FileOutputStream("Users//users.txt", true));
-        f.reset();
+        File f2= new File("Users//users.txt");
+        FileOutputStream f1= new FileOutputStream(f2, true);
+        if(f2.length()==0){
+        ObjectOutputStream f = new ObjectOutputStream(f1);
         f.writeObject(u);
-        f.close();
+        f.close();}
+        else
+        {
+            AppendingObjectOutputStream f = new AppendingObjectOutputStream(f1);
+            f.writeObject(u);
+            f.close();
+        }
     }
 //    void add(String name,String gender,int age, String mail, String username, String password, int ID){
 //        User
